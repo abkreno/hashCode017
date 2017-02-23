2fe58485919913fce9c792619977c1b4253544f8
hamamsy <mohamedelhamamsy789@gmail.com>
2017-02-23 23:02:16 +0200

    Adds solution1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -15,50 +17,49 @@ public class Main {
 		int index;
 		int size;
 		int freq; //
-		TreeMap<Integer, Integer> myEndPoints;
+		LinkedList<Integer> myEndPoints;
 
 		Video(int index, int size) {
 			this.index = index;
 			this.size = size;
-			myEndPoints = new TreeMap<>();
+			myEndPoints = new LinkedList<>();
 		}
 
 		@Override
 		public int compareTo(Video o) {
+			if (freq == o.freq) {
+				return Integer.compare(size, o.size);
+			}
 			return -Integer.compare(freq, o.freq);
 		}
 
-		public void addEndPoint(int epId, int numOfR) {
-			myEndPoints.put(epId, numOfR);
+		public void addEndPoint(int epId) {
+			myEndPoints.add(epId);
 		}
 	}
 
 	static class Cache {
-		LinkedList<Edge> connectedEndPoint;
+		HashMap<Integer, Integer> connectedEndPoint;
 		int index;
+		int updatedSize;
+		double averageLatency;
+		LinkedList<Integer> addedVids;
 
 		Cache(int i) {
 			index = i;
-			connectedEndPoint = new LinkedList<>();
+			connectedEndPoint = new HashMap<>();
+			addedVids = new LinkedList<>();
+			updatedSize = cacheSize;
 		}
 
-	}
-
-	static class Edge {
-		int connectedEp;
-		int latency;
-
-		Edge(int connectedEp, int latency) {
-			this.connectedEp = connectedEp;
-			this.latency = latency;
-		}
 	}
 
 	static class EndPoint {
-		int latencyFromDataCenter;
+		int index, latencyFromDataCenter;
 		TreeMap<Integer, Integer> connectedVideos;
 
-		EndPoint(int latencyFromDataCenter) {
+		EndPoint(int indx, int latencyFromDataCenter) {
+			this.index = indx;
 			this.latencyFromDataCenter = latencyFromDataCenter;
 			this.connectedVideos = new TreeMap<>();
 		}
@@ -69,7 +70,14 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		// BufferedReader bf = new BufferedReader(new
+		// InputStreamReader(System.in));
+		String filename = "kittens";
+		//String filename = "videos_worth_spreading";
+		//String filename = "trending_today";
+		//String filename = "me_at_the_zoo";
+
+		BufferedReader bf = new BufferedReader(new FileReader(filename+".in"));
 		String[] l = bf.readLine().split(" ");
 		numOfVideos = Integer.parseInt(l[0]);
 		numOfEndPoints = Integer.parseInt(l[1]);
@@ -90,12 +98,12 @@ public class Main {
 			l = bf.readLine().split(" ");
 			int latencyFromDataCenter = Integer.parseInt(l[0]);
 			int nc = Integer.parseInt(l[1]);
-			endPoints[i] = new EndPoint(latencyFromDataCenter);
+			endPoints[i] = new EndPoint(i, latencyFromDataCenter);
 			for (int j = 0; j < nc; j++) {
 				l = bf.readLine().split(" ");
 				int cacheId = Integer.parseInt(l[0]);
 				int latencyFromCache = Integer.parseInt(l[1]);
-				caches[cacheId].connectedEndPoint.add(new Edge(i, latencyFromCache));
+				caches[cacheId].connectedEndPoint.put(endPoints[i].index, latencyFromCache);
 			}
 		}
 		for (int i = 0; i < numOfRequests; i++) {
@@ -103,10 +111,68 @@ public class Main {
 			int vidId = Integer.parseInt(l[0]);
 			int epId = Integer.parseInt(l[1]);
 			int numOfR = Integer.parseInt(l[2]);
-			videos[vidId].addEndPoint(epId, numOfR);
+			videos[vidId].addEndPoint(epId);
 			videos[vidId].freq += numOfR;
+			endPoints[epId].connectedVideos.put(vidId, numOfR);
 		}
+		int usedCaches = 0;
 		Arrays.sort(videos);
+		for (int i = 0; i < videos.length; i++) {
+			int bestCacheIndex = -1;
+			int videoIndex = videos[i].index;
+			double bestAverage = Double.NEGATIVE_INFINITY;
+			for (int j = 0; j < caches.length; j++) {
+				if (caches[j].updatedSize < videos[i].size) {
+					continue;
+				}
+				double averageLatency = calculatedAverageLatency(videos[i].index, caches[j],
+						videos[i].myEndPoints);
+				if (averageLatency > bestAverage) {
+					bestAverage = averageLatency;
+					bestCacheIndex = j;
+				}
+			}
+			if (bestCacheIndex != -1) {
+				if (caches[bestCacheIndex].addedVids.size() == 0) {
+					usedCaches++;
+				}
+				caches[bestCacheIndex].addedVids.add(videoIndex);
+				caches[bestCacheIndex].updatedSize -= videos[i].size;
 
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(usedCaches + "\n");
+		for (Cache c : caches) {
+			if (c.addedVids.size() == 0)
+				continue;
+			sb.append(c.index);
+			for (int vidIndex : c.addedVids) {
+				sb.append(" " + vidIndex);
+			}
+			sb.append("\n");
+		}
+		PrintWriter pw = new PrintWriter(new File(filename+".out"));
+		pw.print(sb.toString());
+		pw.flush();
+		pw.close();
+	}
+
+	private static double calculatedAverageLatency(int vidInd, Cache cache, LinkedList<Integer> myEndPoints) {
+		double average = 0, totalRequests = 0;
+		double requests, cacheLatency, saved;
+		EndPoint tmp;
+		for (int endPointInd : myEndPoints) {
+			tmp = endPoints[endPointInd];
+			requests = tmp.connectedVideos.get(vidInd);
+			// TODO check
+			if (!cache.connectedEndPoint.containsKey(tmp.index))
+				continue;
+			cacheLatency = cache.connectedEndPoint.get(tmp.index);
+			saved = tmp.latencyFromDataCenter - cacheLatency;
+			average += (saved * requests);
+			totalRequests += requests;
+		}
+		return average / totalRequests;
 	}
 }
